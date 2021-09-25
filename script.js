d5e0e1f4d80f92be91586803c0285fb7daeb81c0
bamjs <jeevansai.m1997@gmail.com>
2021-09-25 18:05:06 +0530

    first comit

---
+++ script.js
@@ -0,0 +1,7 @@
+function editing(){
+    var rama 
+    rama = document.getElementsById('firsttag').value;
+    console.log(rama)
+    rama.style.fontFamily = ramabadhra
+    document.getElementsById("output1").innerHtml = rama
+}
